1e8046b106d0195593a47b10fc38c63b5b8c14b1
julian-liu <56050491+julian-liu@users.noreply.github.com>
2021-01-26 01:21:52 -0700

    models on 7 features

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from util import get_features_df, get_alias, get_mnemonic, CV_weighted, get_df_from_mnemonics
+import time
 from sklearn.preprocessing import RobustScaler
 import random 
 
@@ -20,6 +20,9 @@ from sklearn.ensemble import StackingRegressor as Stack
 from plot import plot_logs_columns, plot_crossplot
 import plotly.express as px 
 import plotly.io as pio
+
+from util import get_alias, get_mnemonic, CV_weighted, process_las
+
 pio.renderers.default='browser'
 
 with open('data/las_data_DTSM.pickle', 'rb') as f:
@@ -28,130 +31,37 @@ with open('data/las_data_DTSM.pickle', 'rb') as f:
 with open('data/alias_dict.pickle', 'rb') as f:
     alias_dict = pickle.load(f)
 
-#%% create simple DTSM/DTCO model
 
-# # create ['DTCO'] as X and ['DTSM'] as y
-# Xlables = ['DTCO']
-# ylabels = ['DTSM']
-# X = None
-# y = None
+#%% TEST: get_mnemonics_from_df and create data
 
-# for key, df in las_dict.items():
-#     xlabels_alias = []
-#     ylabels_alias = []
+if __name__ == '__main__':
 
-#     for label in Xlables:
-#         for col in df.columns:
-#             mnemonic = get_mnemonic(col, alias_dict=alias_dict)
-#             if (mnemonic==label) and (mnemonic not in xlabels_alias):            
-#                xlabels_alias.append(col)
+    # load las_data_DTSM
+    with open('data/las_data_DTSM.pickle', 'rb') as f:
+        las_data_DTSM = pickle.load(f)
 
-#     for label in ylabels:
-#         for col in df.columns:
-#             mnemonic = get_mnemonic(col, alias_dict=alias_dict)
-#             if (mnemonic==label) and (mnemonic not in ylabels_alias):            
-#                ylabels_alias.append(col)
-
-#     # double check all mnemonic curve data are in df    
-#     if (len(Xlables)==len(xlabels_alias)) and (len(ylabels)==len(ylabels_alias)):
-
-#         # dropna if present
-#         df.dropna(axis=0, inplace=True)
-#         if X is None:
-#             X = df[xlabels_alias].values.reshape(-1,len(Xlables))
-#         else:
-#             X = np.r_[X, df[xlabels_alias].values.reshape(-1,len(Xlables))]
-        
-#         if y is None:
-#             y = df[ylabels_alias].values.reshape(-1,len(ylabels))
-#         else:
-#             y = np.r_[y, df[ylabels_alias].values.reshape(-1,len(ylabels))]
+    # 7 features
+    target_mnemonics = ['DTCO', 'NPHI','RHOB', 'GR', 'CALI', 'RT', 'PEFZ', 'DTSM']        
+    df = None
+    key_list = []
+    for key in las_data_DTSM.keys():
+        print(f'Loading {key}')
+        df_ = process_las().get_df_by_mnemonics(las_data_DTSM[key], target_mnemonics=target_mnemonics, strict_input_output=True)
+        if df_ is not None:
+            key_list.append(key)
+            if df is None:
+                df = df_
+            else:
+                df = pd.concat([df, df_], axis=0)
+    print(f'Total {len(key_list)} las loaded and total {len(df)} rows of data!')
+    with open('data/las_data_7features.pickle', 'wb') as f:
+        pickle.dump(df, f)
 
 
+#%% fit models with 7 features dataset
 
-# print('Data X and y shape:', X.shape, y.shape)
-
-# models = {
-#         'RCV': RCV(), 
-#         'LSVR': LSVR(epsilon=0.1),
-#         'KNN': KNN(n_neighbors=10),
-#         'GBR': GBR(),
-#         'MLP': MLP(hidden_layer_sizes=(10,)),
-#         'XGB': XGB(tree_method='hist', objective='reg:squarederror', n_estimators=100),
-# }
-
-# for name, model in models.items():
-
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)    
-#     scaler_x, scaler_y = RobustScaler(), RobustScaler()
-#     X_train = scaler_x.fit_transform(X_train)
-#     y_train = scaler_y.fit_transform(y_train)
-
-#     reg = model.fit(X_train, y_train)
-
-#     X_test = scaler_x.transform(X_test)
-#     y_predict = scaler_y.inverse_transform(reg.predict(X_test).reshape(-1,1))   
-    
-#     # not the correct way, should scale train and test data seprately
-#     X_ = scaler_x.fit_transform(X)
-#     y_ = scaler_x.fit_transform(y)
-#     text = f'{name} - rmse_cv: {CV_weighted(reg, X_, y_)}'
-    
-#     # plot crossplot
-#     plot_crossplot(y_actual=y_test, 
-#                 y_predict=y_predict,
-#                 text=text,
-#                 plot_show=True,
-#                 plot_return=False,
-#                 plot_save_file_name=f'Prediction-{name}',
-#                 plot_save_path='plots/DTCO-DTSM',
-#                 plot_save_format=['png'])
-   
-
-#%% create model with desired curves
-
-# create ['DTCO'] as X and ['DTSM'] as y
-Xlables = ['DTCO', 'NPHI', 'GR', 'CALI', 'RT']
-ylabels = ['DTSM']
-X = None
-y = None
-
-for key, df in las_dict.items():
-    xlabels_alias = []
-    ylabels_alias = []
-
-    for label in Xlables:
-        for col in df.columns:
-            mnemonic = get_mnemonic(col, alias_dict=alias_dict)
-            if (mnemonic==label) and (mnemonic not in xlabels_alias):            
-               xlabels_alias.append(col)
-
-    for label in ylabels:
-        for col in df.columns:
-            mnemonic = get_mnemonic(col, alias_dict=alias_dict)
-            if (mnemonic==label) and (mnemonic not in ylabels_alias):            
-               ylabels_alias.append(col)
-
-    # double check all desired mnemonic curve data are in df    
-    if (len(Xlables)==len(xlabels_alias)) and (len(ylabels)==len(ylabels_alias)):
-
-        # check if entire column is na
-        for col in df.columns:
-            if df[col].isnull().all():
-                print(f"Warning! {key}[{col}] is all nan! It's still included in dataset")
-        # dropna if present
-        df.dropna(axis=0, inplace=True)
-        if X is None:
-            X = df[xlabels_alias].values.reshape(-1,len(Xlables))
-        else:
-            X = np.r_[X, df[xlabels_alias].values.reshape(-1,len(Xlables))]
-        
-        if y is None:
-            y = df[ylabels_alias].values.reshape(-1,len(ylabels))
-        else:
-            y = np.r_[y, df[ylabels_alias].values.reshape(-1,len(ylabels))]
-
-
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1:]
 
 print('Data X and y shape:', X.shape, y.shape)
 
@@ -165,7 +75,7 @@ models = {
 }
 
 for name, model in models.items():
-
+    time0 = time.time()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)    
     scaler_x, scaler_y = RobustScaler(), RobustScaler()
     X_train = scaler_x.fit_transform(X_train)
@@ -179,85 +89,16 @@ for name, model in models.items():
     # not the correct way, should scale train and test data seprately
     X_ = scaler_x.fit_transform(X)
     y_ = scaler_x.fit_transform(y)
-    text = f'{name} - rmse_cv: {CV_weighted(reg, X_, y_)}'
+    text = f'{name} - rmse_cv: {CV_weighted(reg, X_, y_):.2f}'
     
     # plot crossplot
-    plot_crossplot(y_actual=y_test, 
+    plot_crossplot(y_actual=y_test.values, 
                 y_predict=y_predict,
                 text=text,
                 plot_show=True,
                 plot_return=False,
                 plot_save_file_name=f'Prediction-{name}',
-                plot_save_path='plots/DTCO-DTSM',
+                plot_save_path='predictions/7features',
                 plot_save_format=['png'])
    
-#%% plot all las
-
-if __name__ == '__main__':        
-
-    with open('data/las_data_DTSM.pickle', 'rb') as f:
-        las_dict = pickle.load(f)
-
-    # plot some random las
-    key = random.choice(list(las_dict.keys()))
-    
-    if '001' in key:
-        plot_logs_columns(las_dict[key], 
-                            well_name=key,
-                            plot_show=True)
-                            # plot_return=False,
-                            # plot_save_file_name=key_,
-                            # plot_save_path='plots',
-                            # plot_save_format=['png', 'html'])
-
-    # # plot all las
-    # for key in las_dict.keys():
-        
-    #     plot_logs_columns(las_dict[key], 
-    #                     well_name=key,
-    #                     plot_show=False,
-    #                     plot_return=False,
-    #                     plot_save_file_name=key_,
-    #                     plot_save_path='plots',
-    #                     plot_save_format=['png', 'html'])
-
-#%% test get_mnemonics_from_df
-
-if __name__ == '__main__':
-
-    # load las_data_DTSM
-    with open('data/las_data.pickle', 'rb') as f:
-        las_data = pickle.load(f)
-
-    # load las_data_DTSM
-    with open('data/las_data_DTSM.pickle', 'rb') as f:
-        las_data_DTSM = pickle.load(f)
-
-    key = '001-00a60e5cc262_TGS'
-    df = las_data[key]
-    df.head()
-
-    xlabels_alias = []
-    ylabels_alias = []
-
-    target_mnemonics = ['DTCO', 'NPHI', 'GR', 'CALI', 'RT', 'DTSM']
-
-        
-    df2 = get_df_from_mnemonics(df, target_mnemonics=target_mnemonics)
-
-
-    plot_logs_columns(df, 
-                    plot_show=True, 
-                    DTSM_only=True,
-                    plot_return=False,
-                    plot_save_file_name=f'{key}-rawdata',
-                    plot_save_path='plot_demo',
-                    plot_save_format=['png', 'html'])
-                    
-    plot_logs_columns(df2, 
-                    plot_show=True, 
-                    DTSM_only=True,
-                    plot_return=False,
-                    plot_save_file_name=f'{key}-cleaneddata',
-                    plot_save_path='plot_demo',
-                    plot_save_format=['png', 'html'])
+    print(f'Finished fitting with {name} model in {time.time()-time0:.2f} seconds')
---
+++ test.py
@@ -0,0 +1,191 @@
+import pickle
+
+from plot import plot_logs_columns
+
+#%% plot all las and save the plots
+
+if __name__ == '__main__':        
+
+    with open('data/las_data_DTSM.pickle', 'rb') as f:
+        las_data_DTSM = pickle.load(f)
+
+    # plot some random las
+    key = random.choice(list(las_data_DTSM.keys()))
+
+    plot_logs_columns(las_data_DTSM[key], 
+                        well_name=key,
+                        plot_show=True)                        
+
+    for key in las_data_DTSM.keys():
+        plot_logs_columns(las_data_DTSM[key], 
+                        well_name=key,
+                        plot_show=False,
+                        plot_return=False,
+                        plot_save_file_name=key,
+                        plot_save_path='plots',
+                        plot_save_format=['png', 'html'])
+
+
+#%% create simple DTSM/DTCO model
+
+# # create ['DTCO'] as X and ['DTSM'] as y
+# Xlables = ['DTCO']
+# ylabels = ['DTSM']
+# X = None
+# y = None
+
+# for key, df in las_dict.items():
+#     xlabels_alias = []
+#     ylabels_alias = []
+
+#     for label in Xlables:
+#         for col in df.columns:
+#             mnemonic = get_mnemonic(col, alias_dict=alias_dict)
+#             if (mnemonic==label) and (mnemonic not in xlabels_alias):            
+#                xlabels_alias.append(col)
+
+#     for label in ylabels:
+#         for col in df.columns:
+#             mnemonic = get_mnemonic(col, alias_dict=alias_dict)
+#             if (mnemonic==label) and (mnemonic not in ylabels_alias):            
+#                ylabels_alias.append(col)
+
+#     # double check all mnemonic curve data are in df    
+#     if (len(Xlables)==len(xlabels_alias)) and (len(ylabels)==len(ylabels_alias)):
+
+#         # dropna if present
+#         df.dropna(axis=0, inplace=True)
+#         if X is None:
+#             X = df[xlabels_alias].values.reshape(-1,len(Xlables))
+#         else:
+#             X = np.r_[X, df[xlabels_alias].values.reshape(-1,len(Xlables))]
+        
+#         if y is None:
+#             y = df[ylabels_alias].values.reshape(-1,len(ylabels))
+#         else:
+#             y = np.r_[y, df[ylabels_alias].values.reshape(-1,len(ylabels))]
+
+
+
+# print('Data X and y shape:', X.shape, y.shape)
+
+# models = {
+#         'RCV': RCV(), 
+#         'LSVR': LSVR(epsilon=0.1),
+#         'KNN': KNN(n_neighbors=10),
+#         'GBR': GBR(),
+#         'MLP': MLP(hidden_layer_sizes=(10,)),
+#         'XGB': XGB(tree_method='hist', objective='reg:squarederror', n_estimators=100),
+# }
+
+# for name, model in models.items():
+
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)    
+#     scaler_x, scaler_y = RobustScaler(), RobustScaler()
+#     X_train = scaler_x.fit_transform(X_train)
+#     y_train = scaler_y.fit_transform(y_train)
+
+#     reg = model.fit(X_train, y_train)
+
+#     X_test = scaler_x.transform(X_test)
+#     y_predict = scaler_y.inverse_transform(reg.predict(X_test).reshape(-1,1))   
+    
+#     # not the correct way, should scale train and test data seprately
+#     X_ = scaler_x.fit_transform(X)
+#     y_ = scaler_x.fit_transform(y)
+#     text = f'{name} - rmse_cv: {CV_weighted(reg, X_, y_)}'
+    
+#     # plot crossplot
+#     plot_crossplot(y_actual=y_test, 
+#                 y_predict=y_predict,
+#                 text=text,
+#                 plot_show=True,
+#                 plot_return=False,
+#                 plot_save_file_name=f'Prediction-{name}',
+#                 plot_save_path='plots/DTCO-DTSM',
+#                 plot_save_format=['png'])
+   
+
+#%% create model with desired curves
+
+# # create ['DTCO'] as X and ['DTSM'] as y
+# Xlables = ['DTCO', 'NPHI', 'GR', 'CALI', 'RT']
+# ylabels = ['DTSM']
+# X = None
+# y = None
+
+# for key, df in las_dict.items():
+#     xlabels_alias = []
+#     ylabels_alias = []
+
+#     for label in Xlables:
+#         for col in df.columns:
+#             mnemonic = get_mnemonic(col, alias_dict=alias_dict)
+#             if (mnemonic==label) and (mnemonic not in xlabels_alias):            
+#                xlabels_alias.append(col)
+
+#     for label in ylabels:
+#         for col in df.columns:
+#             mnemonic = get_mnemonic(col, alias_dict=alias_dict)
+#             if (mnemonic==label) and (mnemonic not in ylabels_alias):            
+#                ylabels_alias.append(col)
+
+#     # double check all desired mnemonic curve data are in df    
+#     if (len(Xlables)==len(xlabels_alias)) and (len(ylabels)==len(ylabels_alias)):
+
+#         # check if entire column is na
+#         for col in df.columns:
+#             if df[col].isnull().all():
+#                 print(f"Warning! {key}[{col}] is all nan! It's still included in dataset")
+#         # dropna if present
+#         df.dropna(axis=0, inplace=True)
+#         if X is None:
+#             X = df[xlabels_alias].values.reshape(-1,len(Xlables))
+#         else:
+#             X = np.r_[X, df[xlabels_alias].values.reshape(-1,len(Xlables))]
+        
+#         if y is None:
+#             y = df[ylabels_alias].values.reshape(-1,len(ylabels))
+#         else:
+#             y = np.r_[y, df[ylabels_alias].values.reshape(-1,len(ylabels))]
+
+
+
+# print('Data X and y shape:', X.shape, y.shape)
+
+# models = {
+#         'RCV': RCV(), 
+#         'LSVR': LSVR(epsilon=0.1),
+#         'KNN': KNN(n_neighbors=10),
+#         'GBR': GBR(),
+#         'MLP': MLP(hidden_layer_sizes=(10,)),
+#         'XGB': XGB(tree_method='hist', objective='reg:squarederror', n_estimators=100),
+# }
+
+# for name, model in models.items():
+
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)    
+#     scaler_x, scaler_y = RobustScaler(), RobustScaler()
+#     X_train = scaler_x.fit_transform(X_train)
+#     y_train = scaler_y.fit_transform(y_train)
+
+#     reg = model.fit(X_train, y_train)
+
+#     X_test = scaler_x.transform(X_test)
+#     y_predict = scaler_y.inverse_transform(reg.predict(X_test).reshape(-1,1))   
+    
+#     # not the correct way, should scale train and test data seprately
+#     X_ = scaler_x.fit_transform(X)
+#     y_ = scaler_x.fit_transform(y)
+#     text = f'{name} - rmse_cv: {CV_weighted(reg, X_, y_)}'
+    
+#     # plot crossplot
+#     plot_crossplot(y_actual=y_test, 
+#                 y_predict=y_predict,
+#                 text=text,
+#                 plot_show=True,
+#                 plot_return=False,
+#                 plot_save_file_name=f'Prediction-{name}',
+#                 plot_save_path='plots/DTCO-DTSM',
+#                 plot_save_format=['png'])
+   
