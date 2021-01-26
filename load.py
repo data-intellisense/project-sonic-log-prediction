1fe49c42a106a2ebe0e38f4bdd7da40a764c8c3d
julian-liu <56050491+julian-liu@users.noreply.github.com>
2021-01-25 17:44:57 -0700

    get_mnemonics_from_df

--- load.py
+++ load.py
@@ -1,4 +1,4 @@
-# this file load the each las data to dataframe and save as h5 format file
+#%% this file load the each las data to dataframe and save as h5 format file
 # the resulting file is the .h5 file in /data/ folder.
 
 import glob
@@ -11,9 +11,10 @@ import lasio
 import numpy as np
 import pandas as pd
 
-from util import read_las, process_las
+from plot import plot_logs_columns
+from util import alias_dict, read_las, process_las, get_mnemonic, get_alias
 
-# used log paser from here: https://lasio.readthedocs.io/en/latest
+#%% used log paser from here: https://lasio.readthedocs.io/en/latest
 # read first las file
 las = lasio.read("data/las/00a60e5cc262_TGS.las")
 
@@ -45,45 +46,10 @@ print(las.get_mnemonic_unit())
 print('lat and lon:', las.get_lat_lon())
 print('start-stop depth:', las.get_start_stop())
 
-#%% read all las file to df and store to hdf file format
+#%% read all las files to df, keep all and valid DTSM only, and store to pickle file format
 
-las_data = pd.HDFStore("data/las_data.h5")
-las_curves = pd.HDFStore("data/las_curves.h5")
-cords = []
-
-
-time_start = time.time()
-count_ = 0
-for f in glob.glob("data/las/*.las"):
-    f_name = f.split("/")[-1].split("\\")[-1][:-4]
-    f_name = f"{count_:0>3}-{f_name}"
-    print(f"Loading {count_}th las file: \t\t{f_name}.las")
-    las = read_las(f)
-    
-    las_data[f_name] = las.df()
-    las_curves[f_name] = las.df_curvedata()
-
-    cords.append([f_name]+las.get_lat_lon()+las.get_start_stop())
-
-    count_ +=1
-
-las_data.close()
-las_curves.close()
-
-cords = pd.DataFrame(cords, columns=['Well', 'Lat', 'Lon', 'STRT', 'STOP'])
-cords.to_csv('data/cords.csv')
-
-print(f'cords: \n{cords.head()}')
-print(f"\nSuccessfully loaded total {count_+1} las files!")
-print(f"Total run time: {time.time()-time_start: .2f} seconds")
-        
-# test
-df1 = process_las().keep_valid_DTSM_only(df)
-df1
-
-#%% read all las files to df, keep valid DTSM only, and store to hdf file format
-
-las_data_DTSM = pd.HDFStore("data/las_data_DTSM.h5")
+las_data = dict()
+las_data_DTSM = dict()
 list_no_DTSM = []
 curve_info = []
 
@@ -92,21 +58,26 @@ count_ = 0
 for f in glob.glob("data/las/*.las"):
     f_name = f.split("/")[-1].split("\\")[-1][:-4]
     f_name = f"{count_:0>3}-{f_name}"
-    print(f"Loading {count_}th las file: \t\t{f_name}.las")
+    print(f"Loading {count_:0>3}th las file: \t{f_name}.las")
 
     curve_info.append([f"{count_:0>3}", f_name])
 
     las = read_las(f)    
     df = las.df()
+    
+    # save all data
+    las_data[f_name] = df.copy()
 
-    try:
-        las_data_DTSM[f_name] = process_las().keep_valid_DTSM_only(df)
-    except:
+    if 'DTSM' in df.columns.map(alias_dict):
+        # save only DTSM valid data    
+        las_data_DTSM[f_name] = process_las().keep_valid_DTSM_only(df).copy()      
+    else:
+        # save a list with las names without DTSM and raise warning
+        print(f'{f_name}.las has no DTSM!')
         list_no_DTSM.append(f_name)
-
+        
     count_ +=1
 
-las_data_DTSM.close()
 
 list_no_DTSM = pd.DataFrame(list_no_DTSM, columns=['las without DTSM'])
 list_no_DTSM.to_csv('data/list_no_DTSM.csv', index=True)
@@ -114,16 +85,22 @@ list_no_DTSM.to_csv('data/list_no_DTSM.csv', index=True)
 curve_info = pd.DataFrame(curve_info, columns=['WellNo', 'WellName'])
 curve_info.to_csv('data/curve_info.csv', index=False)
 
+# save las_data
+with open('data/las_data.pickle', 'wb') as f:
+    pickle.dump(las_data, f)
+
+# save las_data_DTSM
+with open('data/las_data_DTSM.pickle', 'wb') as f:
+    pickle.dump(las_data_DTSM, f)
+
 print(f"\nSuccessfully loaded total {count_+1} las files!")
 print(f"Total run time: {time.time()-time_start: .2f} seconds")
 
+# check if nan columns dropped
+key = '001-00a60e5cc262_TGS'
+a = las_data_DTSM[key]
+a[(a.index>5500) & (a.index<8000)]
 
-# convert h5 to pickle
-las_data_DTSM = pd.HDFStore("data/las_data_DTSM.h5")
-las_data_DTSM_ = dict()
-for key in las_data_DTSM.keys():
-    las_data_DTSM_[key] = las_data_DTSM[key]
-with open('data/las_data_DTSM.pickle', 'wb') as f:
-    pickle.dump(las_data_DTSM_, f)
+plot_logs_columns(a, plot_show=True, well_name=key)
 
-las_data_DTSM.close()
+
